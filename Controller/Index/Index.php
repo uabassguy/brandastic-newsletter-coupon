7b2bcf0c620bc0b97af1fb62d8fd73ca20d1834f
Dave Wilson <uabassguy@gmail.com>
2018-07-03 23:37:51 -0700

    Release v1.0.5: Fix controller issues with guest checkout

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -1,113 +1,110 @@
 <?php
-namespace Brandastic\NewsletterCoupon\Controller\Index;
+namespace Brandastic\NewsletterCoupon\Controller\Index {
 
-use Magento\Framework\Data\Form\FormKey;
-use Magento\Catalog\Api\ProductRepositoryInterface;
+    use Magento\Framework\Data\Form\FormKey;
+    use Magento\Catalog\Api\ProductRepositoryInterface;
+    use Magento\Quote\Model\QuoteRepository;
 
-class Index extends \Magento\Framework\App\Action\Action
-{
-    protected $_productSku = 'Simple1';
-
-    /**
-     * @var \Magento\Quote\Model\Quote
-     */
-    protected $_cart;
-
-    /**
-     * @var FormKey
-     */
-    protected $_formKey;
-
-    /**
-     * @var mixed
-     */
-    protected $_session, $_cartSession;
-
-    /**
-     * @var ProductRepositoryInterface
-     */
-    protected $_productRepository;
-
-    /**
-     * Index constructor.
-     * @param \Magento\Framework\App\Action\Context $context
-     */
-    public function __construct(
-        \Magento\Framework\App\Action\Context $context
-    )
+    class Index extends \Magento\Framework\App\Action\Action
     {
-        $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
-        $this->_cartSession = $objectManager->get(\Magento\Checkout\Model\Session::class);
-        $this->_cart = $this->_getQuote();
-        $this->_session = $objectManager->get('Magento\Customer\Model\Session');
-        $this->_formKey = $objectManager->get(FormKey::class);
-        $this->_productRepository = $objectManager->get(ProductRepositoryInterface::class);
-        return parent::__construct($context);
-    }
+        protected $_productSku = 'Simple1';
 
-    public function execute()
-    {
-        $this->addProductToCart();
-        $this->applyPromoCode();
-        $this->_redirect('checkout');
-    }
+        /**
+         * @var QuoteRepository
+         */
+        protected $_quoteRepository;
+        /**
+         * @var \Magento\Quote\Model\Quote
+         */
+        protected $_cart;
 
-    protected function addProductToCart()
-    {
-        try{
-            $product = $this->_productRepository->get($this->_productSku);
-        } catch (\Exception $e)
+        /**
+         * @var FormKey
+         */
+        protected $_formKey;
+
+        /**
+         * @var mixed
+         */
+        protected $_session, $_cartSession;
+
+        /**
+         * @var ProductRepositoryInterface
+         */
+        protected $_productRepository;
+
+        /**
+         * Index constructor.
+         * @param \Magento\Framework\App\Action\Context $context
+         * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+         * @param \Magento\Customer\Model\Session $session
+         * @param FormKey $formKey
+         * @param ProductRepositoryInterface $productRepository
+         * @param \Magento\Checkout\Model\Cart $cart
+         */
+        public function __construct(
+            \Magento\Framework\App\Action\Context $context,
+            \Magento\Framework\View\Result\PageFactory $resultPageFactory,
+            \Magento\Customer\Model\Session $session,
+            FormKey $formKey,
+            ProductRepositoryInterface $productRepository,
+            \Magento\Checkout\Model\Cart $cart
+        )
         {
-            return;
+            parent::__construct($context);
+            $this->_cart = $cart;
+            $this->_session = $session;
+            $this->_formKey = $formKey;
+            $this->_productRepository = $productRepository;
+            return parent::__construct($context);
         }
 
-        $params = array(
-            'form_key' => $this->_formKey->getFormKey(),
-            'product' => $product->getId(),
-            'qty'   => 1
-        );
+        public function execute()
+        {
+            $this->addProductToCart();
+            $this->_redirect('checkout/cart');
+        }
 
-        $data = \Magento\Framework\App\ObjectManager::getInstance()->get(\Magento\Framework\DataObject::class);
-
-        $data->addData($params);
-        try{
-            // check if already in cart before adding
-            if (!$this->_cart->getItemByProduct($product))
-            {
-                $this->_cart->addProduct($product, $data);
+        protected function addProductToCart()
+        {
+            try {
+                $product = $this->_productRepository->get($this->_productSku);
+            } catch (\Exception $e) {
+                return;
             }
-        } catch (\Exception $e)
-        {
-            return;
+
+            $data = new \Magento\Framework\DataObject([
+                'form_key' => $this->_formKey->getFormKey(),
+                'product' => $product->getId(),
+                'qty' => 1
+            ]);
+
+            try {
+                if (!$this->_cart->getQuote()->getItemByProduct($product)) {
+                    $this->_cart->getQuote()->addProduct($product, $data);
+                    $this->applyPromoCode();
+                    $this->_cart->save();
+                }
+            } catch (\Exception $e) {
+                return;
+            }
         }
-    }
 
-    /**
-     * Get the promo code from the customer session and apply it
-     */
-    protected function applyPromoCode()
-    {
-        $couponCode = $this->_getSession()->getData('coupon');
-        if ($couponCode !== NULL)
+        /**
+         * Get the promo code from the customer session and apply it
+         */
+        protected function applyPromoCode()
         {
-            $this->_getQuote()->setCouponCode($couponCode)
-                ->collectTotals()
-                ->save();
+            $couponCode = $this->_session->getData('coupon');
+            if ($couponCode !== NULL) {
+                $this->_cart->getQuote()->setCouponCode($couponCode)
+                    ->collectTotals();
+            }
         }
-    }
 
-    protected function _getSession()
-    {
-        return $this->_session;
-    }
-
-    protected function _getCartSession()
-    {
-        return $this->_cartSession;
-    }
-
-    protected function _getQuote()
-    {
-        return $this->_getCartSession()->getQuote();
+        protected function _getSession()
+        {
+            return $this->_session;
+        }
     }
 }
